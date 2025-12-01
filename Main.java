3e385d56394307ffafa5bf280be13e3a4cfda875
Vanessa Liz <vanessalizsousa1996@gmail.com>
2025-12-01 00:41:08 -0300

    aula 1/17 - observações iniciais de Java

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Vanessa");
+        nomes.add("Pedro");
+        nomes.add("João");
+        nomes.add("Catarina");
+
+        System.out.println(nomes.get(0));
+        System.out.println(nomes.get(1));
+
+        for(int i = 0; i < nomes.size(); i++){
+            System.out.println(nomes.get(i));
+        }
+
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+
+        int contador = 0;
+        while(contador < 10){
+            System.out.println("Está na contagem");
+            contador++;
+        }
+    }
+}
